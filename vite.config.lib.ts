3b2de3e5cc636c72776902744435e61055e9cb96
Francesco Baciu <francesco.baciu@inficon.com>
2026-05-05 13:16:35 +0200

    Bump packages and prepare for npm release; use scoped package name

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [dts({ include: ['src/lib'] })],
+  publicDir: false,
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib/index.ts'),
